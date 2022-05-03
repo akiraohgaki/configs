e1f89af9826f600e8fee5d5607c1898cc6abc627
Akira Ohgaki <akiraohgaki@gmail.com>
2022-05-03 05:27:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ CONFIGS_BASE_URL='https://raw.githubusercontent.com/akiraohgaki/configs/main'
 
 PREFIX='/usr/local'
 
-TEMPLATES_DIR="${HOME}/Templates"
-
 #CONFIG_DIR=''
 FONTS_DIR=''
 if [ "$(echo ${OSTYPE} | grep 'linux')" ]; then
@@ -107,39 +105,6 @@ elif [ "${input_val}" = 'uninstall' ]; then
   sudo n uninstall
   sudo rm -rf /usr/local/n
   sudo_uninstall_file "${PREFIX}/bin/n"
-fi
-
-if [ "$(which npm)" ]; then
-  echo ''
-  echo '================================================'
-  echo 'Do you want to install additional NPM packages ?'
-  echo "(will be installed in $(npm root -g))"
-  echo '================================================'
-  read -p '[y/n/uninstall]: ' input_val
-
-  if [ "${input_val}" = 'y' ]; then
-    sudo npm install \
-      yarn \
-      typescript @types/node \
-      nodemon \
-      prettier \
-      eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin \
-      react react-dom @types/react @types/react-dom eslint-plugin-react eslint-plugin-react-hooks \
-      jest jest-junit jest-mock \
-      -g
-    sudo chown -R $(id -un):$(id -gn) "${HOME}/.npm"
-    sudo chown -R $(id -un):$(id -gn) "${HOME}/.config"
-  elif [ "${input_val}" = 'uninstall' ]; then
-    sudo npm uninstall \
-      yarn \
-      typescript @types/node \
-      nodemon \
-      prettier \
-      eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin \
-      react react-dom @types/react @types/react-dom eslint-plugin-react eslint-plugin-react-hooks \
-      jest jest-junit jest-mock \
-      -g
-  fi
 fi
 
 echo ''
@@ -256,43 +221,4 @@ elif [ "${input_val}" = 'uninstall' ]; then
   uninstall_file "${FONTS_DIR}/UbuntuMono-BI.ttf"
   uninstall_file "${FONTS_DIR}/UbuntuMono-R.ttf"
   uninstall_file "${FONTS_DIR}/UbuntuMono-RI.ttf"
-fi
-
-echo ''
-echo '================================================'
-echo 'Do you want to install config file for Prettier ?'
-echo "(will be installed in ${TEMPLATES_DIR})"
-echo '================================================'
-read -p '[y/n/uninstall]: ' input_val
-
-if [ "${input_val}" = 'y' ]; then
-  install_file_url "${CONFIGS_BASE_URL}/prettier/.prettierrc.json" "${TEMPLATES_DIR}/.prettierrc.json"
-elif [ "${input_val}" = 'uninstall' ]; then
-  uninstall_file "${TEMPLATES_DIR}/.prettierrc.json"
-fi
-
-echo ''
-echo '================================================'
-echo 'Do you want to install config file for ESLint ?'
-echo "(will be installed in ${TEMPLATES_DIR})"
-echo '================================================'
-read -p '[y/n/uninstall]: ' input_val
-
-if [ "${input_val}" = 'y' ]; then
-  install_file_url "${CONFIGS_BASE_URL}/eslint/.eslintrc.json" "${TEMPLATES_DIR}/.eslintrc.json"
-elif [ "${input_val}" = 'uninstall' ]; then
-  uninstall_file "${TEMPLATES_DIR}/.eslintrc.json"
-fi
-
-echo ''
-echo '================================================'
-echo 'Do you want to install config file for TypeScript ?'
-echo "(will be installed in ${TEMPLATES_DIR})"
-echo '================================================'
-read -p '[y/n/uninstall]: ' input_val
-
-if [ "${input_val}" = 'y' ]; then
-  install_file_url "${CONFIGS_BASE_URL}/typescript/tsconfig.json" "${TEMPLATES_DIR}/tsconfig.json"
-elif [ "${input_val}" = 'uninstall' ]; then
-  uninstall_file "${TEMPLATES_DIR}/tsconfig.json"
 fi
